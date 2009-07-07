879b9558cc345145684fc415404c0bbc1fdd0b1a
Jeremy Wells <jeremy@jeremy-laptop.(none)>
2009-07-07 23:02:08 +1200

    Use memcached

--- Page.py
+++ Page.py
@@ -2,34 +2,38 @@ import datetime
 import logging
 from google.appengine.ext import db
 from google.appengine.api import urlfetch
+from google.appengine.api import memcache
 
 class Page(db.Model):
 	date = db.DateTimeProperty(auto_now_add=True)
 	content = db.TextProperty()
 	url = db.StringProperty()
 	
-def fetch(url, timeout = 60):
+def fetch(url, timeout = 0):
 	logging.debug('fetch: ' + url)
-	cached = Page.gql('WHERE url = :1', url).get()
-
-	if(cached):
-		if(cached.date < datetime.datetime.now() - datetime.timedelta(minutes = timeout)):
-			logging.debug('fetch cache expired')
-			cached.delete()
-		else:	
-			logging.debug('fetch from cache')
-			return cached.content
 	
-	try:
-		response = urlfetch.fetch(url)
-		if(response.status_code == 200):
-			result = unicode(response.content, 'utf-8')
-			cached = Page()
-			cached.url = url
-			cached.content = result			
-			cached.put()
-			return response.content
-	except urlfetch.Error, e:
-		return None
+	data = memcache.get(url)
+	if(data is not None): 
+		return data
+	else:
+		if(timeout == 0):
+			page = Page.gql('WHERE url = :1', url).get()
+			if(page): return page.content	
+	
+		try:
+			response = urlfetch.fetch(url)
+			if(response.status_code == 200):
+				result = unicode(response.content, 'utf-8')
+					
+				if(timeout == 0):
+					memcache.add(url, result)
+					page = Page(url=url, content=result)
+					page.put()
+				else:								
+					memcache.add(url, result, timeout * 60)
+					
+				return result
+		except urlfetch.Error, e:
+			return None
 		
 	return None
