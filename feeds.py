cb81aefa3179b8b067a09af6533c86f7381f2f6d
Jeremy Wells <jeremy@jeremy-laptop.(none)>
2009-07-12 16:18:30 +1200

    Changed to trademe2rss url

--- feeds.py
+++ feeds.py
@@ -12,51 +12,6 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 
-class IndexPage(webapp.RequestHandler):
-	def get(self):
-		self.start_time = datetime.datetime.now()
-		self.max_time = datetime.timedelta(seconds=2)
-	
-		url = 'http://www.trademe.co.nz/Browse/CategoryAttributeSearchResults.aspx?keyval=1022858&from=fav&sort_order=expiry_desc&v=List&Y=0'
-		result = Page.fetch(url, 5)
-		
-		if(result):		
-			self.response.headers['Content-Type'] = 'application/rss+xml'
-			items = self.processResults(result)
-			
-			template_values = {
-				'items': items
-			}
-			
-			path = os.path.join(os.path.dirname(__file__), 'index.rss')
-			self.response.out.write(template.render(path, template_values))
-			
-		else:
-			self.response.headers['Content-Type'] = 'text/plain'
-			self.response.out.write('Hello, webapp world!')
-		
-	def processResults(self, result):
-		soup = BeautifulSoup(result)	
-		items = soup.findAll('li', {"class": re.compile('.*listingCard.*')})
-		items = map(self.processResult, items)
-		
-		
-		for item in items:
-			item.load_details()
-			
-			time = datetime.datetime.now()
-			
-			if(time > self.start_time + self.max_time):
-				logging.debug('stop due to time restraint')
-				break			
-		
-		return items
-		
-	def processResult(self, item):
-		titem = TrademeItem.TrademeItem()
-		titem.load_item(item)
-		return titem
-		
 class New(webapp.RequestHandler, Template):
 	def get(self):
 		self.render()
@@ -86,7 +41,7 @@ class Show(webapp.RequestHandler, Template):
 
 
 application = webapp.WSGIApplication(
-												[('/', IndexPage),
+												[('/', New),
 												('/new', New),
 												('/create', Create),
 												('/show/(.*)', Show)],
